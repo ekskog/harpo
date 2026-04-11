4e0f586a3819d4955338ae939dca5062e30a45f3
Tayo Carvalhal <root@hbvu.su>
2026-04-11 10:02:54 +0200

    get image for the colletion at first get

--- harp-ui/src/components/CollectionView.vue
+++ harp-ui/src/components/CollectionView.vue
@@ -112,14 +112,7 @@
                 title="View song image"
                 @click.stop="openSongImageModal(song)"
               >
-                <img
-                  v-if="!songImageErrors[song.id]"
-                  :src="getSongImageUrl(song)"
-                  class="w-4 h-4 object-cover rounded-full"
-                  alt="Song thumbnail"
-                  @error="handleSongThumbnailError(song.id)"
-                />
-                <svg v-else class="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <svg class="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14M15 10h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                 </svg>
               </button>
